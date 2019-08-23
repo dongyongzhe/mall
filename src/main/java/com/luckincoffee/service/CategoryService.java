96262487ce11b6b121ae9df67ab158cedfcdef33
Administrator <58596779@qq.com>
2019-08-23 19:24:25 +0800

    second

--- src/main/java/com/luckincoffee/service/CategoryService.java
+++ src/main/java/com/luckincoffee/service/CategoryService.java
@@ -5,13 +5,9 @@ import com.luckincoffee.pojo.Category;
 import java.util.List;
 
 /**
- * @ProjectName: mall
- * @Package: com.luckincoffee.service
- * @ClassName: CategoryService
  * @Author: dyz
  * @Description: 分类业务逻辑接口
  * @Date: 2019/8/22 17:31
- * @Version: 1.0
  */
 public interface CategoryService {
     List<Category> list();
